d3538197f8aca63c50a36db3c8d65fdaed5e7bb0
Lee Richmond <lrichmond1@bloomberg.net>
2021-03-07 18:54:46 -0500

    Update to latest graphiti_graphql

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   scope path: ApplicationResource.endpoint_namespace, defaults: { format: :jsonapi } do
     resources :tasks
     resources :epics
-    mount VandalUi::Engine, at: '/vandal'
-    scope defaults: { format: :html } do
-      mount GraphitiGraphQL::Engine, at: "/gql"
-    end
+    mount VandalUi::Engine, at: "/vandal"
+    mount GraphitiGraphQL::Engine, at: "/gql"
   end
 end
